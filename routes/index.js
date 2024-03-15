d3d7d116e4f3920be22f79b46018ff7e2e0753f0
Pushkar Raghuwanshi <pushkarraghuwanshi34@gmail.com>
2024-03-16 01:29:51 +0530

    file view feature

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { isUtf8 } = require('buffer');
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
@@ -9,12 +10,16 @@ let globalpath = path.join(__dirname,'../','/public', '/uploads');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const files = fs.readdirSync(globalpath);
-  res.render('index', {files: files});
+  res.render('index', {files: files, filedata: ""});
 });
 
 router.get('/:filename', function(req, res, next) {
+
+  // uploads folder ke andar file ko read krne ke liye or right side me data dikhane ke liye upload folder ke andar ki files ka data is req.params.filename ki help se data dekh pa rahe h
+  const filedata = fs.readFileSync(path.join(globalpath, req.params.filename), "utf-8");
+
   const files = fs.readdirSync(globalpath);
-  res.render('index', {files: files});
+  res.render('index', {files: files, filedata : filedata});
 });
 
 router.post('/createfile', (req, res)=>{
@@ -23,7 +28,7 @@ router.post('/createfile', (req, res)=>{
   const {filename} = req.body; //destructure ye hota h
   fs.writeFileSync(path.join(globalpath,filename),"")
 
-  res.redirect(`${filename}`);
+  res.redirect(`/${filename}`);
 })
 
 module.exports = router;
